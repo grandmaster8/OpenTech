fc25a35a2c361c9ad984d3026d77decc22ecc9b1
Avaja <java9compiler@gmail.com>
2017-02-13 17:48:44 +0400

    change version

--- src/main/java/ot/OpenTechnology.java
+++ src/main/java/ot/OpenTechnology.java
@@ -19,7 +19,7 @@ import javax.script.ScriptEngineManager;
 public class OpenTechnology {
 
 	public final static String MODID = "OpenTechnology";
-	public final static String VERSION = "0.4.0a_build_18";
+	public final static String VERSION = "0.5.0a_build_19";
 
 	public final static ScriptEngine SCRIPT_ENGINE = (new ScriptEngineManager()).getEngineByName("js");
 
