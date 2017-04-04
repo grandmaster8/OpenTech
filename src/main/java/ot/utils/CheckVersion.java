d0588675d5180077d86de04f4189d95a7777f3bf
Avaja <java9compiler@gmail.com>
2017-04-04 20:16:36 +0300

    change CheckVersion

--- src/main/java/ot/utils/CheckVersion.java
+++ src/main/java/ot/utils/CheckVersion.java
@@ -4,8 +4,12 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import ot.OpenTechnology;
 
-import java.io.InputStream;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Avaja on 28.12.2016.
@@ -27,15 +31,27 @@ public class CheckVersion {
     }
 
     private static String getLatestVersion(){
-        try{
-            InputStream in = new URL("https://raw.githubusercontent.com/Avaja/OpenTechnology/master/LATEST_VERSION").openStream();
+        try {
+            HttpsURLConnection connection = (HttpsURLConnection) new URL("https://github.com/Avaja/OpenTechnology/releases").openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-            byte[] buff = new byte[in.available()];
-            in.read(buff);
-
-            return new String(buff, "UTF-8");
-        }catch (Exception e){
-            return null;
+            String input;
+            String all = "";
+            while ((input = br.readLine()) != null){
+                all += input;
+            }
+            br.close();
+            Pattern pattern = Pattern.compile("latest-build\\((.+?)\\)");
+            Matcher matcher = pattern.matcher(all);
+            if(matcher.find()){
+                return matcher.group(1);
+            }
+            return OpenTechnology.VERSION;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return OpenTechnology.VERSION;
     }
 }
